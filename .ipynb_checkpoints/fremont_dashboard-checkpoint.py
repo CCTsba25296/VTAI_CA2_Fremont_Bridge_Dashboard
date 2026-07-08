30d4f61a521727ce8a446c9a3c18ffa6846ac26f
CCTsba25296 <sba25296@student.cct.ie>
2026-07-08 14:53:48 +0100

    Missing String

--- .ipynb_checkpoints/fremont_dashboard-checkpoint.py
+++ .ipynb_checkpoints/fremont_dashboard-checkpoint.py
@@ -350,5 +350,5 @@ elif chart_choice == "Cyclist Traffic by Sidewalk":
 
     st.info(
         "The East sidewalk recorded a higher total cyclist volume than the West sidewalk across the dataset. "
-        "This indicates an imbalance in cyclist usage between the two pathways, potentially indicating a preference among cyclists towards the East sidewalk compared to the West sidewalk.
+        "This indicates an imbalance in cyclist usage between the two pathways, potentially indicating a preference among cyclists towards the East sidewalk compared to the West sidewalk."
     )
